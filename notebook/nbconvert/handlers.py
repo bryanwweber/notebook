225e8dd3a8ac5cc6b3b4f3c5c293c8121ba2f87e
Bryan W. Weber <bryan.w.weber@gmail.com>
2018-04-29 16:52:03 -0400

    Add Python code to do the conversion to PDF and merger

--- notebook/nbconvert/handlers.py
+++ notebook/nbconvert/handlers.py
@@ -19,6 +19,8 @@ from nbformat import from_dict
 from ipython_genutils.py3compat import cast_bytes
 from ipython_genutils import text
 
+from pdfrw import PdfReader, PdfWriter
+
 def find_resource_files(output_files_dir):
     files = []
     for dirpath, dirnames, filenames in os.walk(output_files_dir):
@@ -142,6 +144,88 @@ class NbconvertFileHandler(IPythonHandler):
         self.set_header('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0')
         self.finish(output)
 
+
+class DLconvertFileHandler(IPythonHandler):
+
+    SUPPORTED_METHODS = ('GET',)
+
+    @web.authenticated
+    def get(self, format, path):
+
+        exporter = get_exporter(format, config=self.config, log=self.log)
+        pdfs = []
+
+        path = path.strip('/')
+        paths = path.split('/')
+        for path in paths:
+            self.log.info(path)
+            # If the notebook relates to a real file (default contents manager),
+            # give its path to nbconvert.
+            if hasattr(self.contents_manager, '_get_os_path'):
+                os_path = self.contents_manager._get_os_path(path)
+                ext_resources_dir, basename = os.path.split(os_path)
+            else:
+                ext_resources_dir = None
+
+            model = self.contents_manager.get(path=path)
+            name = model['name']
+            if model['type'] != 'notebook':
+                # not a notebook, redirect to files
+                return FilesRedirectHandler.redirect_to_files(self, path)
+
+            nb = model['content']
+
+            self.set_header('Last-Modified', model['last_modified'])
+
+            # create resources dictionary
+            mod_date = model['last_modified'].strftime(text.date_format)
+            nb_title = os.path.splitext(name)[0]
+
+            resource_dict = {
+                "metadata": {
+                    "name": nb_title,
+                    "modified_date": mod_date
+                },
+                "config_dir": self.application.settings['config_dir']
+            }
+
+            if ext_resources_dir:
+                resource_dict['metadata']['path'] = ext_resources_dir
+
+            try:
+                output, resources = exporter.from_notebook_node(
+                    nb,
+                    resources=resource_dict
+                )
+            except Exception as e:
+                self.log.exception("nbconvert failed: %s", e)
+                raise web.HTTPError(500, "nbconvert failed: %s" % e)
+
+            pdfs.append(io.BytesIO(output))
+
+        writer = PdfWriter()
+        for pdf in pdfs:
+            writer.addpages(PdfReader(pdf).pages)
+        bio = io.BytesIO()
+        writer.write(bio)
+        bio.seek(0)
+        output = bio.read()
+        bio.close()
+
+        # Force download if requested
+        if self.get_argument('download', 'false').lower() == 'true':
+            filename = os.path.splitext(name)[0] + resources['output_extension']
+            self.set_attachment_header(filename)
+
+        # MIME type
+        if exporter.output_mimetype:
+            self.set_header('Content-Type',
+                            '%s; charset=utf-8' % exporter.output_mimetype)
+
+        self.set_header('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0')
+        self.finish(output)
+
+
 class NbconvertPostHandler(IPythonHandler):
     SUPPORTED_METHODS = ('POST',)
 
@@ -183,4 +267,6 @@ default_handlers = [
     (r"/nbconvert/%s" % _format_regex, NbconvertPostHandler),
     (r"/nbconvert/%s%s" % (_format_regex, path_regex),
          NbconvertFileHandler),
+     (r"/dlconvert/%s%s" % (_format_regex, path_regex),
+          DLconvertFileHandler),
 ]
